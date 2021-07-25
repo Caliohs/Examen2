f1302958b83e410249197bc19f0e3f6babf6a6cb
CarlosQ <caliohs@hotmail.com>
2021-07-25 10:27:08 -0600

    Agregar archivos de proyecto.

---
+++ BaseDatos/dbo/StoreProcedures/DireccionObtener.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].DireccionObtener
+@IdDireccion INT=null
+
+AS BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+
+			D.IdDireccion
+		,   D.Estado
+		, D.DireccionExacta
+
+		,   P.IdPersona
+		,	P.Nombre
+
+		,   CP.IdCatalogoProvincia
+		,	CP.NombreCatalogoProvincia
+
+		
+		,   CC.IdCatalogoCanton
+		,	CC.NombreCatalogoCanton
+
+
+		,   CD.IdCatalogoDistrito
+		,	CD.NombreCatalogoDistrito
+	
+		
+
+		
+	
+				
+
+	FROM dbo.Direccion D
+	INNER JOIN dbo.Persona P
+         ON D.IdPersona = P.IdPersona
+	 INNER JOIN dbo.CatalogoProvincia CP
+         ON D.IdCatalogoProvincia = CP.IdCatalogoProvincia
+     INNER JOIN dbo.CatalogoCanton CC
+         ON D.IdCatalogoCanton = CC.IdCatalogoCanton
+	 INNER JOIN dbo.CatalogoDistrito CD
+         ON D.IdCatalogoDistrito = CD.IdCatalogoDistrito
+	 
+	WHERE
+	     (@IdDireccion IS NULL OR IdDireccion=@IdDireccion)
+
+END
